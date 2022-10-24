0445a500a7aa79b3619366574b69d197e211b21b
Winda Sekar Widiati <windasw17@gmail.com>
2022-10-24 09:57:33 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+// untuk menampilkan text
+echo "Hello World! <br>";
+
+// untuk membuat variabel
+$nama = "Winda Sekar Widiati";
+$umur = 21;
+
+// agar variabel muncul di halaman
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+// operasi aritmatika
+$namaAdik = "Adik Nalanda Pranamarta";
+$umurAdik = 11;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun";
+
+?>
