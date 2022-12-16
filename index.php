11b6f5d01e11cf50edac068c16214cb633fe068b
Winda Sekar Widiati <windasw17@gmail.com>
2022-12-16 11:25:04 +0700

    Adding index.php and insert.php files

--- index.php
+++ index.php
@@ -1,23 +1,56 @@
 <?php
+    // membuat koneksi dengan MySQL
+    $con = mysqli_connect("localhost","root","","seal_fakultas");
 
-// untuk menampilkan text
-echo "Hello World! <br>";
+    // check connection with MySQL
+    if (mysqli_connect_errno()) {
+        echo "Koneksi gagal" . mysqli_connect_error();
+        exit();
+    }else{
+        echo "Koneksi berhasil";
+    }
 
-// untuk membuat variabel
-$nama = "Winda Sekar Widiati";
-$umur = 21;
+    // membuat data dari table mysql
+    $query = "SELECT * FROM mahasiswa";
 
-// agar variabel muncul di halaman
-echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+    // menampilkan data, cek apakah query bisa dijalankan
+    $result = mysqli_query($con,$query);
+    $mahasiswa = [];
+    if ($result){
+        // menampilkan satu per satu
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<br>".$row["nama"]."alamat :".$row["alamat"];
+            $mahasiswa[] = $row;
+        }
+        mysqli_free_result($result);
+    }
 
-// operasi aritmatika
-$namaAdik = "Adik Nalanda Pranamarta";
-$umurAdik = 11;
-
-echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
-
-$selisihUmur = $umur - $umurAdik;
-
-echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun";
+    // menutup koneksi
+    mysqli_close($con);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <?php var_dump($mahasiswa);?>
+    <table border="1" style="width:100%;">
+    <tr>
+        <th>NIM</th>
+        <th>Nama</th>
+    </tr>
+    <?php foreach($mahasiswa as $value): ?>
+    <tr>
+        <td><?php echo $value["nim"]; ?></td>
+        <td><?php echo $value["nama"]; ?></td>
+    </tr>
+    <?php endforeach; ?>
+</body>
+</html>
